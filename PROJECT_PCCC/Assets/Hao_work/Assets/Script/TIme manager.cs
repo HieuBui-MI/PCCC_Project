d21d2f14cfa3af82bfc5d4b9e90230c49870ebd7
haonuongbo <haoduy396@gmail.com>
2025-04-02 09:31:07 +0700

    Add clock

---
+++ PROJECT_PCCC/Assets/Hao_work/Assets/Script/TIme manager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private const float REAL_SECONDS_PER_INGAME_DAY = 60f;
+    public Text timeText;
+    private float day;
+    private void Awake()
+    {
+        timeText = transform.Find("timeText").GetComponent<Text>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float hoursPerDay = 24f;
+        day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
+        float dayNormalized = day % 1f;
+        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
+
+        float minutesPerhour = 60f;
+        string minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f)* minutesPerhour).ToString("00");
+
+        timeText.text = hoursString + ":" + minutesString; 
+    }
+}
