693c2cc68c39da024a0f2b6aeaef38de55e5e8f3
haonuongbo <haoduy396@gmail.com>
2025-04-02 15:21:18 +0700

    added time

--- PROJECT_PCCC/Assets/Hao_work/Assets/Script/TIme manager.cs
+++ PROJECT_PCCC/Assets/Hao_work/Assets/Script/TIme manager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    private const float REAL_SECONDS_PER_INGAME_DAY = 60f;
+    public float REAL_SECONDS_PER_INGAME_DAY = 60f;
     public Text timeText;
     private float day;
     private void Awake()
@@ -18,9 +18,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
       float hoursPerDay = 24f;
+
         day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
+
+
         float dayNormalized = day % 1f;
-        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
+        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("14");
 
         float minutesPerhour = 60f;
         string minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f)* minutesPerhour).ToString("00");
